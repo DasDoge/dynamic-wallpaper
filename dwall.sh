194d0907f9e32d603eaf9df00ee84f398e0c3206
Colin <32263284+DasDoge@users.noreply.github.com>
2020-06-27 01:07:49 +0200

    use pywal as wallpaper setter and simplified/cleaned up a little

--- dwall.sh
+++ dwall.sh
@@ -17,98 +17,23 @@ W='\033[1;37m'
 # zsh fix
 set -o shwordsplit 2>/dev/null
 
-# set wallpaper in kde
-setkde() {
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "
-    var allDesktops = desktops();
-    print (allDesktops);
-    for (i=0;i<allDesktops.length;i++) {
-        d = allDesktops[i];
-        d.wallpaperPlugin = 'org.kde.image';
-        d.currentConfigGroup = Array('Wallpaper',
-                                    'org.kde.image',
-                                    'General');
-        d.writeConfig('Image', 'file://"$1"')
-    }"
-}
-
-case "$OSTYPE" in
-	linux*)
-			DIR="/usr/share/dynamic-wallpaper"
-			;;
-	*)
-			DIR="/usr/share/dynamic-wallpaper"
-			;;
-esac
-
-case "$OSTYPE" in
-	linux*)
-			TIME="$(date +%k)"
-			;;
-	*)
-			TIME="$(date +%k)"
-			;;
-esac
-
-## For XFCE
-if [[ "$OSTYPE" == "linux"* ]]; then
-	SCREEN="$(xrandr --listactivemonitors | awk -F ' ' 'END {print $1}' | tr -d \:)"
-    MONITOR="$(xrandr --listactivemonitors | awk -F ' ' 'END {print $2}' | tr -d \*+)"
-fi
-
-case "$OSTYPE" in
-	linux*)
-			if [ -n "$SWAYSOCK" ]; then
-				SETTER="eval ogurictl output '*' --image";
-			elif [[ "$DESKTOP_SESSION" =~ ^(MATE|Mate|mate)$ ]]; then 
-				SETTER="gsettings set org.mate.background picture-filename";
-			elif [[ "$DESKTOP_SESSION" =~ ^(Xfce Session|xfce session|XFCE|xfce|Xubuntu|xubuntu)$ ]]; then 
-				SETTER="xfconf-query --channel xfce4-desktop --property /backdrop/screen$SCREEN/monitor$MONITOR/workspace0/last-image --set";
-			elif [[ "$DESKTOP_SESSION" =~ ^(LXDE|Lxde|lxde)$ ]]; then 
-				SETTER="pcmanfm --set-wallpaper";
-            elif [[ "$DESKTOP_SESSION" =~ ^(/usr/share/xsessions/plasma|NEON|Neon|neon|PLASMA|Plasma|plasma|KDE|Kde|kde)$ ]]; then 
-				SETTER=setkde;
-			elif [[ "$DESKTOP_SESSION" =~ ^(PANTHEON|Pantheon|pantheon|GNOME|Gnome|gnome|Gnome-xorg|gnome-xorg|UBUNTU|Ubuntu|ubuntu|DEEPIN|Deepin|deepin|POP|Pop|pop)$ ]]; then 
-				SETTER="gsettings set org.gnome.desktop.background picture-uri";
-			else 
-				SETTER="feh --bg-scale"; 
-			fi
-			;;
-	*)
-			if [ -n "$SWAYSOCK" ]; then
-				SETTER="eval ogurictl output '*' --image";
-			elif [[ "$DESKTOP_SESSION" =~ ^(MATE|Mate|mate)$ ]]; then 
-				SETTER="gsettings set org.mate.background picture-filename";
-			elif [[ "$DESKTOP_SESSION" =~ ^(Xfce Session|xfce session|XFCE|xfce|Xubuntu|xubuntu)$ ]]; then 
-				SETTER="xfconf-query --channel xfce4-desktop --property /backdrop/screen$SCREEN/monitor$MONITOR/workspace0/last-image --set";
-			elif [[ "$DESKTOP_SESSION" =~ ^(LXDE|Lxde|lxde)$ ]]; then 
-				SETTER="pcmanfm --set-wallpaper";
-            elif [[ "$DESKTOP_SESSION" =~ ^(/usr/share/xsessions/plasma|NEON|Neon|neon|PLASMA|Plasma|plasma|KDE|Kde|kde)$ ]]; then 
-				SETTER=setkde;
-			elif [[ "$DESKTOP_SESSION" =~ ^(PANTHEON|Pantheon|pantheon|GNOME|Gnome|gnome|Gnome-xorg|gnome-xorg|UBUNTU|Ubuntu|ubuntu|DEEPIN|Deepin|deepin|POP|Pop|pop)$ ]]; then 
-				SETTER="gsettings set org.gnome.desktop.background picture-uri";
-			else 
-				SETTER="feh --bg-scale"; 
-			fi
-			;;			
-esac
-
-STYLE=
-RUN_ONCE=false
+DIR="/usr/share/dynamic-wallpaper";
+TIME="$(date +%k)";
+SETTER="wal -qi";
 
 set_wallpaper() {
-  image="$DIR/images/$STYLE/$1"
-  if [ -f "$image.png" ]; then
+	image="$DIR/images/$STYLE/$1"
+	if [ -f "$image.png" ]; then
 		FORMAT="png"
-  elif [ -f "$image.jpg" ]; then
+	elif [ -f "$image.jpg" ]; then
 		FORMAT="jpg"
-  else
+	else
 		echo -e $R"Invalid theme/style, Try again."; exit 1;
-  fi
-  if [ $FORMAT ]; then
+	fi
+	if [ $FORMAT ]; then
 		$SETTER "$image.$FORMAT"
 		return;
-  fi
+	fi
 }
 
 main() {
@@ -129,7 +54,7 @@ usage() {
 	echo -e $M"-o  style     Run once and exit, useful for schedulers (cron)."
 	echo -e $M"-h  help      show this usage/help message."
 	echo
- 	echo -e $C"Styles Dir: $DIR/images"
+	echo -e $C"Styles Dir: $DIR/images"
 	echo
 	printf $G"Available styles/themes:  "
 	printf -- $Y'%s  ' "${available_styles[@]}"
@@ -141,17 +66,17 @@ init() {
 		main;
 		reset_color;
 		exit 0;
-    fi
-    while true; do
-		main && sleep 60 && exec $DIR/dwall.sh -s $STYLE
-    done
+	fi
+	while true; do
+		$DIR/dwall.sh -o $STYLE && sleep 60;
+	done
 }
 
 is_valid_style() {
-    available_styles=($(ls $DIR/images))
-    for i in "${available_styles[@]}"; do
-        [ "$i" == "$1" ] && STYLE=$(echo "$1")
-    done
+	available_styles=($(ls $DIR/images))
+	for i in "${available_styles[@]}"; do
+		[ "$i" == "$1" ] && STYLE=$(echo "$1")
+	done
 }
 
 reset_color() {
@@ -162,34 +87,35 @@ reset_color() {
 is_valid_style "$1"
 
 while getopts ":s:o:h" opt; do
-  case ${opt} in
-    s )
+	case ${opt} in
+	s )
+		RUN_ONCE=false
 		STYLE=$OPTARG
 		;;
-    o )
+	o )
 		RUN_ONCE=true
 		STYLE=$OPTARG
 		;;
-    h )
+	h )
 		usage
 		reset_color
 		exit 0
 		;;
-    \?)
+	\?)
 		echo -e $R"Unknown option,$G run dwall -h"
 		reset_color
 		exit 1
 		;;
-    : )
+	: )
 		echo -e $R"Invalid:$G -$OPTARG$R requires an argument."
 		reset_color
 		exit 1
 		;;
-  esac
+	esac
 done
 
 if [ "$STYLE" ]; then
-    init
+	init
 else
 	usage
 	reset_color
